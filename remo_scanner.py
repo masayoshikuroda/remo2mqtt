c3ff834c205306759b4cf30ed2fb48f962fb6c88
masayoshikuroda <kuroda.masayoshi@gmail.com>
2025-02-25 19:11:28 +0900

    Rapis対応

--- remo_scanner.py
+++ remo_scanner.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import logging
 import json
@@ -27,12 +28,16 @@ class RemoScanner:
 
     def get_devices(self):
         logging.info("Retrieving devices...")
-        return self.get(self.baseurl + '/1/devices')
+        devices = self.get(self.baseurl + '/1/devices')
+        logger.debug(json.dumps(devices, ensure_ascii=False, indent=2))
+        return devices
     
     def get_appliances(self):
         logging.info("Retrieving appliances...")
-        return self.get(self.baseurl + '/1/appliances')
-    
+        appliances = self.get(self.baseurl + '/1/appliances')
+        logger.debug(json.dumps(appliances, ensure_ascii=False, indent=2))
+        return appliances
+ 
     def scan(self, detection_callback):
         logger.info("Scanning Remo information...")
         devices = self.get_devices()
@@ -46,14 +51,18 @@ class RemoScanner:
                 info['serial_number'] = device['serial_number']
 
                 newest_events = device['newest_events']
-                info['temperature'] = newest_events['te']['val']
-                info['temperature_at'] = newest_events['te']['created_at']
-                info['humidity'] = newest_events['hu']['val']
-                info['humidity_at'] = newest_events['hu']['created_at']
-                info['illuminance'] = newest_events['il']['val']
-                info['illuminance_at'] = newest_events['il']['created_at']
-                info['movement'] = newest_events['mo']['val']
-                info['movement_at'] = newest_events['mo']['created_at']
+                if 'te' in newest_events:
+                    info['temperature'] = newest_events['te']['val']
+                    info['temperature_at'] = newest_events['te']['created_at']
+                if 'hu' in newest_events:
+                    info['humidity'] = newest_events['hu']['val']
+                    info['humidity_at'] = newest_events['hu']['created_at']
+                if 'il' in newest_events:
+                    info['illuminance'] = newest_events['il']['val']
+                    info['illuminance_at'] = newest_events['il']['created_at']
+                if 'mo' in newest_events:
+                    info['movement'] = newest_events['mo']['val']
+                    info['movement_at'] = newest_events['mo']['created_at']
 
                 detection_callback(info)
 
@@ -78,3 +87,27 @@ class RemoScanner:
 
                 detection_callback(info)
 
+    def get_homeassitatnt_config(topic, data):
+        config = {}
+        config['name'] = f"{data['name']} {data['mac_address']} Temperature"
+        config['state_topic'] = f"{topic}/{data['address']}/info"
+        config['device_class'] = 'temperature'
+        config['device'] = {}
+        config['device']['manufacturer'] = 'Nature Inc.'
+        config['device']['model'] = data['name']
+        config['device']['serial_number'] = data['serial_number']
+        config['value_template'] = '{{ value_json.temperature }}'
+        config['suggested_display_precision'] = 1
+        config['unit_of_measurement']  = '°C'
+        return config
+
+def _detection_callback(info):
+    print('====== detection callback ======')
+    print(json.dumps(info, ensure_ascii=True, indent=2))
+
+if __name__ == "__main__":
+    logger.setLevel(logging.INFO)
+    REMO_URL = os.environ.get('REMO_URL', 'https://api.nature.global')
+    REMO_TOKEN = os.environ.get('REMO_TOKEN', 'token')
+    remo = RemoScanner(REMO_URL, REMO_TOKEN)
+    remo.scan(_detection_callback)
